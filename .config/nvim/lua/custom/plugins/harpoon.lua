a73b8639b97f777fa5f9b73df419040cea5a7276
Dmytro Nakonechnyi <dmytr0x@icloud.com>
2024-06-29 16:37:49 +0100

    [vim] haproon keymaps

--- .config/nvim/lua/custom/plugins/harpoon.lua
+++ .config/nvim/lua/custom/plugins/harpoon.lua
@@ -5,8 +5,28 @@ return {
     require("telescope").load_extension("harpoon")
   end,
   keys = {
-    vim.keymap.set("n", "<leader>hm", ":Telescope harpoon marks<CR>", { desc = "Harpoon [M]arks" }),
-    vim.keymap.set("n", "<leader>ha", ":lua require('harpoon.mark').add_file()<CR>", { desc = "Harpoon [A]dd File" }),
+    vim.keymap.set("n", "<leader>ha", function()
+      require("harpoon.mark").add_file()
+    end, { desc = "Harpoon [A]dd File" }),
+
+    vim.keymap.set("n", "<leader>hm", function()
+      require("telescope").extensions.harpoon.marks({
+        layout_strategy = "vertical",
+        prompt_title = "Harpoon Marks",
+        results_title = "Marks",
+        preview_title = "Preview",
+      })
+    end, { desc = "Harpoon [M]arks" }),
+
+    vim.keymap.set("n", "<leader>hl", function()
+      local ui = require("harpoon.ui")
+      ui.nav_next()
+    end, { desc = "Harpoon [N]ext" }),
+
+    vim.keymap.set("n", "<leader>hh", function()
+      local ui = require("harpoon.ui")
+      ui.nav_prev()
+    end, { desc = "Harpoon [P]revious" }),
   },
   lazy = true,
 }
--- .config/nvim/lua/custom/plugins/noice.lua
+++ .config/nvim/lua/custom/plugins/noice.lua
@@ -8,16 +8,20 @@ return {
   config = function()
     require("noice").setup({
       presets = {
-        long_message_to_split = true, -- long messages will be sent to a split
+        -- long messages will be sent to a split
+        long_message_to_split = true,
       },
-      -- routes = {
-      --   filter = { event = "notify", find = "No information available" },
-      --   opts = { skip = true },
-      -- },
+      -- it seems that it does not work
+      routes = {
+        filter = { event = "notify", find = "No information available" },
+        opts = { skip = true },
+      },
     })
 
     require("notify").setup({
       background_colour = "#000000",
     })
+
+    require("telescope").load_extension("noice")
   end,
 }
