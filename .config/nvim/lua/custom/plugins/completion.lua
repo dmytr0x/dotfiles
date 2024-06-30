a4a3014462297dbc16c0747a9b26c12b1df43e39
Dmytro Nakonechnyi <dmytr0x@icloud.com>
2024-06-30 21:43:21 +0100

    [vim] disable automatic auto complete popup from show up

--- .config/nvim/lua/custom/plugins/completion.lua
+++ .config/nvim/lua/custom/plugins/completion.lua
@@ -73,7 +73,7 @@ return { -- Autocompletion
           luasnip.lsp_expand(args.body)
         end,
       },
-      completion = { completeopt = "menu,menuone,noinsert" },
+      completion = { autocomplete = false, completeopt = "menu,menuone,noinsert" },
 
       -- For an understanding of why these mappings were
       -- chosen, you will need to read `:help ins-completion`
@@ -92,7 +92,8 @@ return { -- Autocompletion
         -- Accept ([y]es) the completion.
         --  This will auto-import if your LSP supports it.
         --  This will expand snippets if the LSP sent a snippet.
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        -- ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
 
         -- If you prefer more traditional completion keymaps,
         -- you can uncomment the following lines
