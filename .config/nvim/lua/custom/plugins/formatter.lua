8825cdf93eb7ce7c39c733e1d18ef4a12bf15c78
Dmytro Nakonechnyi <dmytr0x@icloud.com>
2024-07-14 22:23:13 +0100

    [vim] setup treesitter autotag & formatter

---
+++ .config/nvim/lua/custom/plugins/formatter.lua
@@ -0,0 +1,51 @@
+return { -- Autoformat
+  "stevearc/conform.nvim",
+  lazy = false,
+  keys = {
+    {
+      "<leader>cf",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      mode = { "n", "v" },
+      desc = "[F]ormat",
+    },
+  },
+  opts = {
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      -- Disable "format_on_save lsp_fallback" for languages that don't
+      -- have a well standardized coding style. You can add additional
+      -- languages here or re-enable it for the disabled ones.
+      local disable_filetypes = {
+        c = true,
+        cpp = true,
+      }
+
+      return {
+        timeout_ms = 500,
+        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+      }
+    end,
+    formatters_by_ft = {
+      -- Conform can also run multiple formatters sequentially
+      --
+      lua = { "stylua" },
+      python = { "ruff_format" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
+      json = { "prettier" },
+      yaml = { "prettier" },
+      markdown = { "prettier" },
+    },
+    formatters = {
+      stylua = {
+        prepend_args = { "--indent-type=Spaces", "--indent-width=2" },
+      },
+    },
+  },
+}
