e5fdc7a076330bf221da7f8621c23e5f5dbf4d15
Dmytro Nakonechnyi <dmytr0x@icloud.com>
2025-01-31 20:58:41 +0000

    [zsh] add installation of oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -8,9 +8,20 @@ source ./core.sh
 
 info "🚀 Installing dotfiles ..."
 
-source ./scripts/setup/symlink.sh
-brew bundle --no-upgrade --file=./scripts/setup/Brewfile
-source ./scripts/setup/python.sh
-source ./scripts/setup/javascript.sh
-source ./scripts/setup/rust.sh
-source ./scripts/setup/golang.sh
+# Create symlinks
+# source ./scripts/setup/symlink.sh
+
+# Install packages via homebrew
+# brew bundle --no-upgrade --file=./scripts/setup/Brewfile
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install oh-my-zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install programming languages
+# source ./scripts/setup/python.sh
+# source ./scripts/setup/javascript.sh
+# source ./scripts/setup/rust.sh
+# source ./scripts/setup/golang.sh
