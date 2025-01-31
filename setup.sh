da5d86a3e8b5ca8c16a33c7859ca5457dfeee2a7
Dmytro Nakonechnyi <dmytr0x@icloud.com>
2025-01-31 21:11:13 +0000

    [zsh] all installation steps are activated

--- setup.sh
+++ setup.sh
@@ -9,10 +9,10 @@ source ./core.sh
 info "🚀 Installing dotfiles ..."
 
 # Create symlinks
-# source ./scripts/setup/symlink.sh
+source ./scripts/setup/symlink.sh
 
 # Install packages via homebrew
-# brew bundle --no-upgrade --file=./scripts/setup/Brewfile
+brew bundle --no-upgrade --file=./scripts/setup/Brewfile
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -21,7 +21,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Install programming languages
-# source ./scripts/setup/python.sh
-# source ./scripts/setup/javascript.sh
-# source ./scripts/setup/rust.sh
-# source ./scripts/setup/golang.sh
+source ./scripts/setup/python.sh
+source ./scripts/setup/javascript.sh
+source ./scripts/setup/rust.sh
+source ./scripts/setup/golang.sh
