84468e3fee4be001276ce14fd9d39b03f89de2f4
Dmytro Nakonechnyi <dmytr0x@icloud.com>
2025-04-11 21:15:47 +0100

    [yazi] add command to open file/dir via GUI editor

--- scripts/setup/symlink.sh
+++ scripts/setup/symlink.sh
@@ -35,4 +35,3 @@ symlink $(pwd)/dotfiles/bin/flush-dns $HOME/.local/bin/flush-dns
 symlink $(pwd)/dotfiles/bin/_dup_scout.py $HOME/.local/bin/_dup_scout.py
 symlink $(pwd)/dotfiles/bin/_pdf_slicer.py $HOME/.local/bin/_pdf_slicer.py
 symlink $(pwd)/dotfiles/bin/remove-all $HOME/.local/bin/remove-all
-symlink $(pwd)/dotfiles/bin/gui-editor $HOME/.local/bin/gui-editor
