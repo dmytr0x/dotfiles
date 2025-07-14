70af980fa3c3344f8021b39e88591a57baa68b3d
Dmytro Nakonechnyi <dmytr0x@icloud.com>
2025-07-14 23:22:47 +0100

    [dotfiles] improve mise integration

--- scripts/setup/python.sh
+++ scripts/setup/python.sh
@@ -5,13 +5,3 @@ source ./core.sh
 
 # Install global python
 mise install python
-
-# basedpyright
-BASEDPYRIGHT_VERSION=$(get_last_brew_package_version "basedpyright")
-info "🚀 Installing basedpyright $BASEDPYRIGHT_VERSION ..."
-brew install --quiet basedpyright
-
-# ruff
-RUFF_VERSION=$(get_last_brew_package_version "ruff")
-info "🚀 Installing ruff $RUFF_VERSION ..."
-brew install --quiet ruff
