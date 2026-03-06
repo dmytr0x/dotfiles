48318c5204cc88da8a582285fc3c557968394327
Dmytro Nakonechnyi <dmytr0x@icloud.com>
2026-03-06 17:18:00 +0000

    [dotfiles] speed up the opening of new shell

--- sources/symlinks/home/.config/zsh/completions.sh
+++ sources/symlinks/home/.config/zsh/completions.sh
@@ -1,4 +1,11 @@
 # Load Git completion
 zstyle ':completion:*:*:git:*' script $HOME/.config/zsh/completions/git.sh
 fpath=($HOME/.config/zsh/completions $fpath)
-autoload -Uz compinit && compinit
+autoload -Uz compinit
+# If ~/.zcompdump is missing or older than 24 hours, rebuild completions.
+# Otherwise, load completions from the existing dump without checking it
+if [[ ! -f ~/.zcompdump || -n ~/.zcompdump(#qN.mh+24) ]]; then
+  compinit
+else
+  compinit -C
+fi
