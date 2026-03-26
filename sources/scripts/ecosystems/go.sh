c9f57f8385de38e6075b67dd33af2f10c43373bd
Dmytro Nakonechnyi <dmytr0x@Mac.fritz.box>
2026-03-26 12:48:34 +0000

    [dotfiles] improve the installation scripts

--- sources/scripts/ecosystems/go.sh
+++ sources/scripts/ecosystems/go.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 echo "🚀 Installing golang..."
 mise install go
--- sources/scripts/ecosystems/rust.sh
+++ sources/scripts/ecosystems/rust.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 echo "🚀 Installing rust ..."
 mise install rust
 
 echo "🚀 Installing rust-analyzer ..."
-cargo install rust-analyzer
+mise exec --quiet rust -- cargo install rust-analyzer
