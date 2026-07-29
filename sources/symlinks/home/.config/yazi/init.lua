b910145edb03a0dffbb58ef84b5812c5f78d59c9
Dmytro Nakonechnyi <dmytr0x@icloud.com>
2026-07-29 17:32:38 +0100

    [yazi] update plugins

--- sources/symlinks/home/.config/yazi/init.lua
+++ sources/symlinks/home/.config/yazi/init.lua
@@ -1,2 +1,5 @@
--- plugin: https://github.com/yazi-rs/plugins/tree/main/git.yazi
-require("git"):setup()
+-- Git status signs from https://github.com/yazi-rs/plugins/tree/main/git.yazi
+require("git"):setup {
+	-- Higher-priority linemode children are rendered first.
+	order = 1500,
+}
